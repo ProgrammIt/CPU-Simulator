a3fad277ebadfe186cc83a578e6f9e8a3ae7664b
Erik Burmester <erik.burmester@nextbeam.net>
2024-10-24 14:47:32 +0200

    Implement Singleton pattern.

--- src/simulator/Assembler.ts
+++ src/simulator/Assembler.ts
@@ -1,11 +1,18 @@
 class Assembler {
     private _langDefinition: Object;
-    private _instance: Assembler;
+    private static _instance: Assembler | null | undefined = null;
 
     private constructor() {};
     private split(): string {};
     private parse(): Bit[][] {};
-    public get instance(): Assembler {};
+
+    public static get instance(): Assembler {
+        if (Assembler._instance == null || Assembler._instance == undefined) {
+            Assembler._instance = new Assembler();
+        }
+        return Assembler._instance;
+    }
+    
     public loadLanguageDefinition(f: File): void {};
     public compile(f: File): void {};
 }
