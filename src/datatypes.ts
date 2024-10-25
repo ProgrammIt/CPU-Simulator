1b97411076a6a9ebac8dfe9977714ceca774d558
Erik Burmester <erik.burmester@nextbeam.net>
2024-10-25 14:49:35 +0200

    Define datatypes including Bit, Word and DoubleWord

---
+++ src/datatypes.ts
@@ -0,0 +1,57 @@
+type Bit = 0|1;
+
+type Byte = [Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit];
+
+type Word = [
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit
+];
+
+type DoubleWord = [
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit,
+  Bit
+];
