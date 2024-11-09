c316881ddc75337a9943aaf45b62b1d2aa1b84b1
Erik Burmester <erik.burmester@nextbeam.net>
2024-11-09 21:34:19 +0100

    Update name of function to fit prupose more

--- src/helper.ts
+++ src/helper.ts
@@ -3,20 +3,20 @@ import { BINARY_DATA_REGEX, HEX_DATA_REGEX } from "./constants";
 /**
  * This method converts a given memory address in hexadecimal, binary or decimal representation 
  * to an address in binary representation.
- * @param address A hexadecimal, binary or decimal value representing a memory address.
+ * @param value A hexadecimal, binary or decimal value representing a memory address.
  * @returns The binary representation of the given memory address.
  */
-export function convertToBinaryAddress(address: string): string {
+export function convertToBinaryValue(value: string): string {
 	var convertedValue: string = "";
-	if (address.match(BINARY_DATA_REGEX)) {
+	if (value.match(BINARY_DATA_REGEX)) {
 		// New value gets interpreted as a binary value and passed on as is.
-		convertedValue = address;
-	} else if (address.match(HEX_DATA_REGEX)) {
+		convertedValue = value;
+	} else if (value.match(HEX_DATA_REGEX)) {
 		// New value gets interpreted as a hexadecimal value and parsed to a decimal value.
-		convertedValue = parseInt(address, 16).toString(2);
+		convertedValue = parseInt(value, 16).toString(2);
 	} else {
 		// New value gets interpreted as a decimal value and parsed to a decimal value.
-		convertedValue = parseInt(address, 10).toString(2);
+		convertedValue = parseInt(value, 10).toString(2);
 	}
 	return convertedValue;
 }
