8cd3ac468f8b3a9a47c96dc4cd2620b8a3aef3d1
Erik Burmester <erik.burmester@nextbeam.net>
2024-11-09 14:22:49 +0100

    Use Electron API to communicate with main process and retrieve main memory contents

--- src/render.ts
+++ src/render.ts
@@ -1,59 +1,9 @@
-window.onload = () => {
-    const ramCells: Map<string,string> = new Map<string, string>([
-        ["0x0", "11011001"],
-        ["0x1", "00101110"],
-        ["0x2", "10100001"],
-        ["0x3", "01100000"],
-        ["0x4", "00000000"],
-        ["0x5", "00000000"],
-        ["0x6", "00000000"],
-        ["0x7", "01100100"],
-        ["0x8", "00000000"],
-        ["0x9", "00000000"],
-        ["0xA", "00000000"],
-        ["0xB", "00000000"],
-        ["0xC", "10011000"],
-        ["0xD", "00101110"],
-        ["0xE", "10100001"],
-        ["0xF", "01100000"],
-        ["0x10", "00000000"],
-        ["0x11", "00000000"],
-        ["0x12", "00000000"],
-        ["0x13", "00000001"],
-        ["0x14", "00000000"],
-        ["0x15", "00000000"],
-        ["0x16", "00000000"],
-        ["0x17", "00000000"],
-        ["0x18", "10011000"],
-        ["0x19", "01111110"],
-        ["0x1A", "10100001"],
-        ["0x1B", "01100000"],
-        ["0x1C", "00000000"],
-        ["0x1D", "00000000"],
-        ["0x1E", "00000000"],
-        ["0x1F", "00000000"],
-        ["0x20", "00000000"],
-        ["0x21", "00000000"],
-        ["0x22", "00000000"],
-        ["0x23", "00000000"],
-        ["0x24", "11111000"],
-        ["0x25", "11101110"],
-        ["0x26", "11100001"],
-        ["0x27", "00000000"],
-        ["0x28", "00000000"],
-        ["0x29", "00000000"],
-        ["0x2A", "00000000"],
-        ["0x2B", "00000101"],
-        ["0x2C", "00000000"],
-        ["0x2D", "00000000"],
-        ["0x2E", "00000000"],
-        ["0x2F", "00000000"]
-    ]);
-
-    createRAMCells(ramCells);
+window.onload = async () => {
+    const ramCells: Map<string,string> = await window.mainMemory.cells();
+    renderRAMCells(ramCells);
 }
 
-function createRAMCells(ram: Map<string,string>) {
+function renderRAMCells(ram: Map<string,string>) {
     if (ram === undefined || ram === null) {
         throw Error("RAM object is null");
     }
@@ -68,27 +18,31 @@ function createRAMCells(ram: Map<string,string>) {
     if (ramCellsHTMLElement !== null) {
 
         for (let [key, value] of Array.from(ram).reverse()) {
-            let labelRamCellHTMLElement: HTMLElement = document.createElement("label");
-            let divRamCellHTMLElement: HTMLElement = document.createElement("div");
+            const labelRamCellHTMLElement: HTMLElement = document.createElement("label");
+            const divRamCellHTMLElement: HTMLElement = document.createElement("div");
+            const diffCurrentAndLatestAddressDec = parseInt(lastHexAddress, 16) - parseInt(key, 16);
 
-            let diffCurrentAndLatestHexAddress = parseInt(lastHexAddress, 16) - parseInt(key, 16);
-
-            if (lastHexAddress !== "" && diffCurrentAndLatestHexAddress > 1) {
+            if (lastHexAddress.length !== 0 && diffCurrentAndLatestAddressDec > 1) {
                 // Adding placeholders in the view as an indicator for empty and thus skipped memory cells
-                labelRamCellHTMLElement.innerHTML = "...";
-                labelRamCellHTMLElement.setAttribute("class", "ram-spacer");
+                const labelSpacerHTMLElement: HTMLElement = document.createElement("label");
+                labelSpacerHTMLElement.innerHTML = "...";
+                labelSpacerHTMLElement.setAttribute("class", "ram-spacer");
 
-                divRamCellHTMLElement.innerHTML = "...";
-                divRamCellHTMLElement.setAttribute("class", "ram-spacer");
-            } else {
-                labelRamCellHTMLElement.innerHTML = key;
-                labelRamCellHTMLElement.setAttribute("for", `ram-cell-${key}`);
-                labelRamCellHTMLElement.setAttribute("class", "ram-cell-label");
-                
-                divRamCellHTMLElement.innerHTML = value;
-                divRamCellHTMLElement.setAttribute("id", `ram-cell-${key}`);
-                divRamCellHTMLElement.setAttribute("class", "ram-cell");                
+                const divSpacerHTMLElement: HTMLElement = document.createElement("div");
+                divSpacerHTMLElement.innerHTML = "...";
+                divSpacerHTMLElement.setAttribute("class", "ram-spacer");
+
+                ramCellsHTMLElement.appendChild(labelSpacerHTMLElement);
+                ramCellsHTMLElement.appendChild(divSpacerHTMLElement);
             }
+            
+            labelRamCellHTMLElement.innerHTML = key;
+            labelRamCellHTMLElement.setAttribute("for", `ram-cell-${key}`);
+            labelRamCellHTMLElement.setAttribute("class", "ram-cell-label");
+                
+            divRamCellHTMLElement.innerHTML = value;
+            divRamCellHTMLElement.setAttribute("id", `ram-cell-${key}`);
+            divRamCellHTMLElement.setAttribute("class", "ram-cell");
             
             ramCellsHTMLElement.appendChild(labelRamCellHTMLElement);
             ramCellsHTMLElement.appendChild(divRamCellHTMLElement);
