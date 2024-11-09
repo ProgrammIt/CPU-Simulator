47a7080c0dc3fe3095ee91d14f22df27e7861c88
Erik Burmester <erik.burmester@nextbeam.net>
2024-11-09 21:47:50 +0100

    Change everything from quadword to doubleword

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { MainMemory } from "./simulator/functional_units/MainMemory";
 import path from 'path';
 import { Assembler } from "./simulator/Assembler";
 import { readFileSync } from "fs";
-import { QUADWORD_WIDTH } from "./constants";
+import { DOUBLEWORD } from "./constants";
 
 const createWindow = () => {
   	const win = new BrowserWindow({
@@ -18,15 +18,15 @@ const createWindow = () => {
 };
 
 app.whenReady().then(() => {
-	const mainMemory = MainMemory.instance(Math.pow(2, QUADWORD_WIDTH));
+	const mainMemory = MainMemory.instance(Math.pow(2, DOUBLEWORD));
 	const assembler = Assembler.instance;
 	assembler.loadLanguageDefinition(readFileSync("./src/settings/language_definition.json", "utf-8"));
 	const assemblyProgram: string = readFileSync("./src/assets/programs/examples/loop.asm", "utf8");
 	const compiledProgram: string[] = assembler.compile(assemblyProgram);
 	const startAddressProgrammDec: number = 0;
 	var currentAddressDec: number = startAddressProgrammDec;
-	for (const quadword of compiledProgram) {
-		mainMemory.writeQuadwordTo((currentAddressDec).toString(2), quadword);
+	for (const doubleword of compiledProgram) {
+		mainMemory.writeQuadwordTo((currentAddressDec).toString(2), doubleword);
 		currentAddressDec += 4;
 	}
 	var physicalAddress: string = "";
