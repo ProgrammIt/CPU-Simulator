9ba341324405865a48e9058b6c39d7c11f4be28b
Erik Burmester <erik.burmester@nextbeam.net>
2024-11-09 21:45:45 +0100

    Add constant for size of word, doubleword and quadword

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
-export const QUADWORD_WIDTH: number = 32;
+export const WORD: number = 16;
+export const DOUBLEWORD: number = 32;
+export const QUADWORD: number = 64;
 export const BINARY_DATA_REGEX: RegExp = /^(?:0b)?[01]*$/gmi;
 export const HEX_DATA_REGEX: RegExp = /^0x[1-9ABCDEF][0-9ABCDEF]*$/gmi;
