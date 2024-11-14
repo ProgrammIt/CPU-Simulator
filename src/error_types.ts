93bec690d0ac385839666a816972aa26889349a1
Erik Burmester <erik.burmester@nextbeam.net>
2024-11-14 21:09:19 +0100

    Add error class representing an overflow.

---
+++ src/error_types.ts
@@ -0,0 +1,11 @@
+export class OverflowError extends Error {
+	/**
+	 * Constructs a new instance of the OverflowError with the given message text.
+	 * @param msg A message text.
+	 */
+	constructor(msg: string) {
+		super(msg);
+        // Set the prototype explicitly to enable typechecking.
+        Object.setPrototypeOf(this, OverflowError.prototype);
+    }
+}
