70f9effe287d778e531b926e357e36977bc862bc
Erik Burmester <erik.burmester@nextbeam.net>
2024-11-15 02:23:04 +0100

    Delete some contents from file in order to create a enum from them

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,3 @@
-export const BYTE: number = 8;
-export const WORD: number = 16;
-export const DOUBLEWORD: number = 32;
-export const QUADWORD: number = 64;
 export const BINARY_DATA_REGEX: RegExp = /^(?:0b)?[01]*$/gmi;
 export const HEX_DATA_REGEX: RegExp = /^0x[1-9ABCDEF][0-9ABCDEF]*$/gmi;
+export const DEC_DATA_REGEX: RegExp = /^[1-9][0-9]*$/gmi;
