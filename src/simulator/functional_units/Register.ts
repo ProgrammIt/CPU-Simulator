59f7225fa3afa4d0583a4a8058a8134c271dca6c
Erik Burmester <erik.burmester@nextbeam.net>
2024-11-16 02:06:17 +0100

    Add comments and refactor

--- src/simulator/functional_units/Register.ts
+++ src/simulator/functional_units/Register.ts
@@ -1,52 +1,49 @@
-import { convertToBinaryValue } from "../../helper";
+import { Byte, Doubleword } from "../../types";
 
 export class Register {
-    private _capacity: number;
-    private _value: string;
-    private _name: string;
+    /**
+     * The name of the register.
+     * @readonly
+     */
+    public readonly name: string;
 
     /**
      * This method constructs an instance of the Register class.
-     * @param capacity The capacaty of the register.
+     * @param name The name of the register.
      */
-    public constructor(name: string, capacity: number) {
-        this._name = name.toUpperCase();
-        this._capacity = capacity;
-        this._value = " ";
+    public constructor(name: string) {
+        this.name = name.toUpperCase();
     }
+}
+
+export class ByteRegister extends Register {
+    /**
+     * The registers content.
+     */
+    public content: Byte;
 
     /**
-     * Getter for the registers name.
+     * This method constructs an instance.
+     * @param name The name of the register.
      */
-    public get name(): string {
-        return this._name;
+    public constructor(name: string) {
+        super(name.toUpperCase());
+        this.content = new Byte();
     }
+}
+
+export class DoublewordRegister extends Register {
+    /**
+     * The registers content.
+     */
+    public content: Doubleword;
 
     /**
-     * Getter for the registers capacity.
+     * This method constructs an instance.
+     * @param name The name of the register.
      */
-    public get capacity(): number {
-        return this._capacity;
-    }
-
-    /**
-     * Getter for the current hold value of the register.
-     */
-    public get value(): string {
-        return this._value;
-    }
-
-    /**
-     * Setter for manipulating the hold value of the register.
-     * @param n The new value to save in the register.
-     */
-    public set value(n: string) {
-        // Make sure that new value is not empty.
-        if (n.length === 0) {
-            throw Error("No value given to save in the register");
-        }
-        // Make sure, that only a binary value is saved to the register.
-        this._value = convertToBinaryValue(n);
-        return;
+    public constructor(name: string) {
+        super(name.toUpperCase());
+        this.content = new Doubleword();
     }
 }
