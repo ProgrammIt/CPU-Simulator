a63d6bdd49fcc2e0b4ff175f4636d4466630d325
Erik Burmester <erik.burmester@nextbeam.net>
2024-11-24 01:26:54 +0100

    Fix bugs in tests for the Assembler class

--- src/tests/Assembler.test.ts
+++ src/tests/Assembler.test.ts
@@ -1,52 +1,55 @@
-// import {describe, expect, test} from '@jest/globals';
-// import { readFileSync } from 'fs';
-// import { Assembler } from '../simulator/Assembler';
-// import { Doubleword } from '../types';
+import { readFileSync } from 'fs';
+import { Assembler } from '../simulator/Assembler';
+import { Doubleword } from '../types/Doubleword';
 
-// describe('Encode instructions', () => {
-//     var assembler = Assembler.instance;
-//     assembler.loadLanguageDefinition(readFileSync("./src/settings/language_definition.json", "utf-8"));
+describe('Encode instructions', () => {
+    var assembler = Assembler.instance;
+    assembler.loadLanguageDefinition(readFileSync("./src/settings/language_definition.json", "utf-8"));
 
-//     test('Encode instruction "ADD $1, %eax"', () => {
-//         const result: Doubleword = assembler.compile("ADD $1, %eax")[0];
-//         expect(result.toString()).toBe("10011000000011101010000101100000");
-//     });
+    test('Encode instruction "ADD $1, %eax"', () => {
+        const result: Doubleword = assembler.compile("ADD $1, %eax")[0];
+        expect(result.toString()).toBe("10011000000011101010000101100000");
+    });
 
-//     test('Encode instruction "MOV $0x64, %eax"', () => {
-//         const result: Doubleword = assembler.compile("MOV $0x64, %eax")[0];
-//         expect(result.toString()).toBe("11011001001011101010000101100000");
-//     });
+    test('Encode instruction "MOV $0x64, %eax"', () => {
+        const result: Doubleword = assembler.compile("MOV $0x64, %eax")[0];
+        expect(result.toString()).toBe("11011001001011101010000101100000");
+    });
 
-//     test('Encode instruction "NOP"', () => {
-//         const result: Doubleword = assembler.compile("NOP")[0];
-//         expect(result.toString()).toBe("10011111111111100000000100000000");
-//     });
-// });
+    test('Encode instruction "NOP"', () => {
+        const result: Doubleword = assembler.compile("NOP")[0];
+        expect(result.toString()).toBe("10011111111111100000000100000000");
+    });
 
-// describe("Encode assembly programs", () => {
-//     var assembler = Assembler.instance;
-//     assembler.loadLanguageDefinition(readFileSync("./src/settings/language_definition.json", "utf-8"));
-    
-//     var loopTestProgram: string = "MOV $0x64, %eax      ; Kopiere den Wert 100 in das Register eax.\n\n.loop: \n\tSUB $1, %eax     ; Subtrahiere den Wert 1 vom Wert im Register eax.\n\tCMP $0, %eax     ; Vergleiche den Inhalt von Register eax mit der 0.\n\tJG loop         ; Springe zum Schleifenanfang, wenn der Wert im Register ax noch größer als 0 ist.\n\n; Ende des Programms. Das Ergebnis steht in Register eax.";
-//     var expectedOutputLoopProgram: string[] = [
-//         "11011001001011101010000101100000",
-//         "00000000000000000000000001100100",
-//         "00000000000000000000000000000000",
+    test("Encode assembly programs", () => {
+        var assembler = Assembler.instance;
+        assembler.loadLanguageDefinition(readFileSync("./src/settings/language_definition.json", "utf-8"));
+        
+        var loopTestProgram: string = "MOV $0x64, %eax      ; Kopiere den Wert 100 in das Register eax.\n\n.loop: \n\tSUB $1, %eax     ; Subtrahiere den Wert 1 vom Wert im Register eax.\n\tCMP $0, %eax     ; Vergleiche den Inhalt von Register eax mit der 0.\n\tJG loop         ; Springe zum Schleifenanfang, wenn der Wert im Register ax noch größer als 0 ist.\n\n; Ende des Programms. Das Ergebnis steht in Register eax.";
+        var expectedOutputLoopProgram: string[] = [
+            "11011001001011101010000101100000",
+            "00000000000000000000000001100100",
+            "00000000000000000000000000000000",
 
-//         "10011000001011101010000101100000",
-//         "00000000000000000000000000000001",
-//         "00000000000000000000000000000000",
+            "10011000001011101010000101100000",
+            "00000000000000000000000000000001",
+            "00000000000000000000000000000000",
 
-//         "10011000011111101010000101100000",
-//         "00000000000000000000000000000000",
-//         "00000000000000000000000000000000",
+            "10011000011111101010000101100000",
+            "00000000000000000000000000000000",
+            "00000000000000000000000000000000",
 
-//         "11111000111011101110000100000000",
-//         "00000000000000000000000000000101",
-//         "00000000000000000000000000000000"
-//     ];
-    
-//     test("Encode assembly program with a simple loop", () => {
-//         expect(assembler.compile(loopTestProgram)).toEqual(expectedOutputLoopProgram);
-//     });
-// });
+            "11111000111011101110000100000000",
+            "00000000000000000000000000000101",
+            "00000000000000000000000000000000"
+        ];
+
+        const compiledProgramm: Doubleword[] = assembler.compile(loopTestProgram);
+        const stringyfiedCompiledProgramm: string[] = [];
+        compiledProgramm.forEach(doubleword => {
+            stringyfiedCompiledProgramm.push(doubleword.toString());
+        });
+        
+        expect(stringyfiedCompiledProgramm).toEqual(expectedOutputLoopProgram);
+    });
+});
