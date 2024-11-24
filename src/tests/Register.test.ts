292cdbbe9962337f07c132c4334688bbeef28a7f
Erik Burmester <erik.burmester@nextbeam.net>
2024-11-24 01:28:09 +0100

    Fix imports and bugs in test cases

--- src/tests/Register.test.ts
+++ src/tests/Register.test.ts
@@ -1,10 +1,10 @@
-import { DoublewordRegister, Register } from "../simulator/functional_units/Register";
-import { Bit } from "../types";
+import { DoublewordRegister } from "../simulator/functional_units/Register";
+import { Bit } from "../types/Bit";
 
 describe("Read and write from or to main memory", () => {
     const eax: DoublewordRegister = new DoublewordRegister("EAX");
     test("Write doubleword to register", () => {        
-        eax.content.value = new Array<Bit>(32).fill(new Bit(1));  
+        eax.content.value = new Array<Bit>(32).fill(1);  
         expect(eax.content.value.join("")).toBe("11111111111111111111111111111111");
     });
 
