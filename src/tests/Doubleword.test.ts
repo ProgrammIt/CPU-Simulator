3c774fc4832ff84576202af0a6e314ae9b629b61
Erik Burmester <erik.burmester@nextbeam.net>
2024-11-24 01:25:28 +0100

    Add tests for class method fromInteger to ensure, that positive and negative integer values get converted into their binary representation properly

---
+++ src/tests/Doubleword.test.ts
@@ -0,0 +1,22 @@
+import { Doubleword } from "../types/Doubleword";
+
+describe("Create doubleword from decimal integer values", () => {
+    test("Create doubleword from decimal -6", () => {
+        expect(Doubleword.fromInteger(-6, true).value.join("")).toBe("11111111111111111111111111111010");
+    });
+
+    test("Create doubleword from decimal -300", () => {
+        expect(Doubleword.fromInteger(-300, true).value.join("")).toBe("11111111111111111111111011010100");
+    });
+
+    test("Trigger error because of converting a too large negative decimal integer", () => {
+        const attemptToConvert = () => {
+            Doubleword.fromInteger(-6_000_000_000_000, true);
+        } 
+        expect(attemptToConvert).toThrow(Error);
+    });
+
+    test("Create doubleword from positive decimal integer value", () => {
+        expect(Doubleword.fromInteger(6, true).value.join("")).toBe("00000000000000000000000000000110");
+    });
+});
