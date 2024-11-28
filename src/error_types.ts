bba40b39daf129d587107309b156f93d41cfaa29
Erik Burmester <erik.burmester@nextbeam.net>
2024-11-28 21:25:37 +0100

    Introduce new Error type

--- src/error_types.ts
+++ src/error_types.ts
@@ -9,3 +9,15 @@ export class OverflowError extends Error {
         Object.setPrototypeOf(this, OverflowError.prototype);
     }
 }
+
+export class DivisionByZeroError extends Error {
+	/**
+	 * Constructs a new instance of the OverflowError with the given message text.
+	 * @param msg A message text.
+	 */
+	constructor(msg: string) {
+		super(msg);
+        // Set the prototype explicitly to enable typechecking.
+        Object.setPrototypeOf(this, OverflowError.prototype);
+    }
+}
