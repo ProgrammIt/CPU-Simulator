a54da79dbdcda2ee49689caf3f6b7404e2f06b75
Erik Burmester <erik.burmester@nextbeam.net>
2024-12-03 14:07:44 +0100

    Add methods for retrieving register contents from renderer process

--- src/preload.ts
+++ src/preload.ts
@@ -1,11 +1,28 @@
 import { contextBridge, ipcRenderer } from 'electron';
+import { NumberSystem } from './types';
 
 declare global {
 	interface Window {
-    mainMemory: any
-  }
+		mainMemory: any,
+		simulator: any
+  	}
 }
 
 contextBridge.exposeInMainWorld("mainMemory", {
-  cells: () => ipcRenderer.invoke("retrieveMainMemoryCells"),
+  	readCells: () => ipcRenderer.invoke("retrieveMainMemoryCells"),
+});
+
+contextBridge.exposeInMainWorld("simulator", {
+	nextCycle: () => ipcRenderer.invoke("nextCycle"),
+	readEAX: (basis: NumberSystem = 16) => ipcRenderer.invoke("readEAX", basis),
+	readEBX: (basis: NumberSystem = 16) => ipcRenderer.invoke("readEBX", basis),
+	readEIP: (basis: NumberSystem = 16) => ipcRenderer.invoke("readEIP", basis),
+	eflags: (basis: NumberSystem = 16) => ipcRenderer.invoke("readEFLAGS", basis),
+	readEIR: () => ipcRenderer.invoke("readEIR"),
+	readNPTP: (basis: NumberSystem = 16) => ipcRenderer.invoke("readNPTP", basis),
+	readVMPTR: (basis: NumberSystem = 16) => ipcRenderer.invoke("readVMPTR", basis),
+	readESP: (basis: NumberSystem = 16) => ipcRenderer.invoke("readESP", basis),
+	readITP: (basis: NumberSystem = 16) => ipcRenderer.invoke("readITP", basis),
+	readGPTP: (basis: NumberSystem = 16) => ipcRenderer.invoke("readGPTP", basis),
+	readPTP: (basis: NumberSystem = 16) => ipcRenderer.invoke("readPTP", basis)
 });
