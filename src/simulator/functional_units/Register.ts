ce61ff8ffc411ebd011ce4b157e7e1f201ccc2a1
Erik Burmester <erik.burmester@nextbeam.net>
2024-12-03 14:11:31 +0100

    Add abstract methods for reading and writing the registers content

--- src/simulator/functional_units/Register.ts
+++ src/simulator/functional_units/Register.ts
@@ -2,11 +2,11 @@
  * This class represents a generic register.
  * @author Erik Burmester <erik.burmester@nextbeam.net>
  */
-export class Register<T> {
+export abstract class Register<T> {
     /**
      * The registers content.
      */
-    public content: T;
+    protected _content: T;
 
     /**
      * The name of the register.
@@ -18,9 +18,21 @@ export class Register<T> {
      * This method constructs an instance of the Register class.
      * @param name The name of the register.
      * @param content The initial content of the register.
+     * @constructor
      */
     public constructor(name: string, content: T) {
         this.name = name.toUpperCase();
-        this.content = content;
+        this._content = content;
     }
+
+    /**
+     * Accessor for retrieving a copy of the current registers content.
+     * @returns A copy of the current registers content.
+     */
+    public abstract get content(): T;
+
+    /**
+     * Accessor for setting a new value as registers content.
+     */
+    public abstract set content(newValue: T);
 }
