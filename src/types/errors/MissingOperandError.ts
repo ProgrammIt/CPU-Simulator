a5bb13e0456b17bcc3942406e346b5012e1e5256
Erik Burmester <erik.burmester@nextbeam.net>
2025-01-03 17:57:30 +0100

    Introduce an dedicated error class indicating a missing operand.

---
+++ src/types/errors/MissingOperandError.ts
@@ -0,0 +1,15 @@
+/**
+ * Error which gets thrown whenever an instructions operand is missing.
+ * @author Erik Burmester <erik.burmester@nextbeam.net>
+ */
+export class MissingOperandError extends Error {
+    /**
+	 * Constructs a new instance with the given message.
+	 * @param description A short text describing the error and its cause.
+	 */
+    constructor(description: string) {
+		super(description);
+        // Set the prototype explicitly to enable typechecking.
+        Object.setPrototypeOf(this, MissingOperandError.prototype);
+    }
+}
