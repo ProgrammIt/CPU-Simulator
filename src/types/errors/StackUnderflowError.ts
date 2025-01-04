7b1e5b767323ba8acffa7b2029554f2c091a403b
Erik Burmester <erik.burmester@nextbeam.net>
2025-01-04 16:04:05 +0100

    Add StackUnderflowError as a meaningful extension to the standard error.

---
+++ src/types/errors/StackUnderflowError.ts
@@ -0,0 +1,15 @@
+/**
+ * Error which gets thrown whenever a process tries to pop from STACK but STACK is empty.
+ * @author Erik Burmester <erik.burmester@nextbeam.net>
+ */
+export class StackUnderflowError extends Error {
+    /**
+	 * Constructs a new instance with the given message.
+	 * @param description A short text describing the error and its cause.
+	 */
+    constructor(description: string) {
+		super(description);
+        // Set the prototype explicitly to enable typechecking.
+        Object.setPrototypeOf(this, StackUnderflowError.prototype);
+    }
+}
