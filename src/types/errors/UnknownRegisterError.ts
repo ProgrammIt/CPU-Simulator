3cb5dba8c89b98f66568585c36d900f76ae36b69
Erik Burmester <erik.burmester@nextbeam.net>
2025-01-04 16:04:21 +0100

    Add UnkownRegisterError as a meaningful extension to the standard error.

---
+++ src/types/errors/UnknownRegisterError.ts
@@ -0,0 +1,15 @@
+/**
+ * Error which gets thrown whenever a process tries to access an unkown register.
+ * @author Erik Burmester <erik.burmester@nextbeam.net>
+ */
+export class UnknownRegisterError extends Error {
+    /**
+	 * Constructs a new instance with the given message.
+	 * @param description A short text describing the error and its cause.
+	 */
+    constructor(description: string) {
+		super(description);
+        // Set the prototype explicitly to enable typechecking.
+        Object.setPrototypeOf(this, UnknownRegisterError.prototype);
+    }
+}
