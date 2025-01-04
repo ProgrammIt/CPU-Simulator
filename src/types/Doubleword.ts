03ab156765d75b1cddcaa29d0245a23b72434062
Erik Burmester <erik.burmester@nextbeam.net>
2025-01-04 16:11:46 +0100

    Refactor class to comply with changes in the DataSizes enumeration class and add some new methods to enable fast checks of the binary value

--- src/types/Doubleword.ts
+++ src/types/Doubleword.ts
@@ -1,14 +1,16 @@
-import { DataSize } from "../types";
+import { twosComplementToDecimal } from "../helper";
 import { BinaryValue } from "./BinaryValue";
 import { Bit } from "./Bit";
+import { DataSizes } from "./DataSizes";
 
 export class Doubleword extends BinaryValue {
-	public static readonly MAX_POSITIVE_NUMBER_SIGNED: number = Math.pow(2, DataSize.DOUBLEWORD - 1) - 1;
-	public static readonly MAX_NEGATIVE_NUMBER_SIGNED: number = -1 * Math.pow(2, DataSize.DOUBLEWORD - 1);
-	public static readonly MAX_NUMBER_UNSIGNED: number = Math.pow(2, DataSize.DOUBLEWORD);
+	public static readonly MAX_POSITIVE_NUMBER_SIGNED: number = Math.pow(2, DataSizes.DOUBLEWORD - 1) - 1;
+	public static readonly MAX_NEGATIVE_NUMBER_SIGNED: number = -1 * Math.pow(2, DataSizes.DOUBLEWORD - 1);
+	public static readonly MAX_NUMBER_UNSIGNED: number = Math.pow(2, DataSizes.DOUBLEWORD);
 
 	/**
 	 * Instantiates a new object.
+	 * @param value The initial value of the doubleword.
 	 * @constructor
 	 */
 	public constructor(
@@ -40,20 +42,71 @@ export class Doubleword extends BinaryValue {
 	 * @override
 	 */
 	public set value(newValue: Array<Bit>) {
-		if (newValue.length != DataSize.DOUBLEWORD) {
-			throw new Error(`A new value must have exactly ${DataSize.DOUBLEWORD} bits: ${newValue.length} given.`);
+		if (newValue.length != DataSizes.DOUBLEWORD) {
+			throw new Error(`A new value must have exactly ${DataSizes.DOUBLEWORD} bits: ${newValue.length} given.`);
 		}
 		this._value = newValue.slice();
 	}
 
 	/**
+	 * This method checks whethter the current binary value is equal to the given one or not.
 	 * For comparison, both binary values are converted to strings.
 	 * Conversion presarves the order of items, which is important for the comparison.
 	 * @param doubleword The binary value to compare to.
-	 * @returns True, when both binary values are identical, false otherwise.
+	 * @returns True, if both binary values are identical, false otherwise.
 	 */
 	public equal(doubleword: Doubleword): boolean {
-		return doubleword._value.toString() === this._value.toString();
+		return doubleword.toString() === this.toString();
+	}
+
+	/**
+	 * This method checks whether the current binary value is smaller than the given one.
+	 * @param other The binary value to compare to.
+	 * @returns True, if this value is less than the one compared to, false otherwise.
+	 */
+    public isSmallerThan(other: Doubleword): boolean {
+		return twosComplementToDecimal(this) < twosComplementToDecimal(other);
+    }
+
+	/**
+	 * This method checks whether the current binary value is greater than the given one.
+	 * @param other The binary value to compare to.
+	 * @returns True, if this value is greater than the one compared to, false otherwise.
+	 */
+	public isGreaterThan(other: Doubleword): boolean {
+		return twosComplementToDecimal(this) > twosComplementToDecimal(other);
+	}
+
+	/**
+	 * This method checks whether the current binary value is a binary zero or not.
+	 * @returns True, if the binary value is zero, false otherwise.
+	 */
+	public isZero(): boolean {
+		return this.equal(new Doubleword());
+	}
+
+	/**
+	 * This method checks whether the current binary value is not a binary zero or not.
+	 * @returns True, if the binary value is not zero, false otherwise.
+	 */
+	public isNotZero(): boolean {
+		return !this.isZero();
+	}
+
+	/**
+	 * This method checks whether this binary value represents a negative number.
+	 * @returns True, if the most significant bit is set to 1, false otherwise.
+	 */
+	public isNegative(): boolean {
+		return this._value[0] === 1;
+	}
+
+	/**
+	 * This method checks whether this binary value represents a positive number.
+	 * @returns True, if the most significant bit is set to 0, false otherwise.
+	 */
+	public isPositive(): boolean {
+		return this._value[0] === 0;
 	}
 
 	/**
@@ -80,13 +133,15 @@ export class Doubleword extends BinaryValue {
 		}
 
 		if (integer < Doubleword.MAX_NEGATIVE_NUMBER_SIGNED || integer > Doubleword.MAX_POSITIVE_NUMBER_SIGNED) {
-			throw new Error(`The given number cannot be expressed using ${DataSize.DOUBLEWORD} bits, if the most significant bit should be treated as the sign bit.`);
+			throw new Error(`The given number cannot be expressed using ${DataSizes.DOUBLEWORD} bits, if the most significant bit should be treated as the sign bit.`);
 		}
 
 		var doubleword: Doubleword = new Doubleword();
 
 		// A bit shift converts the given number to a signed 32-bit value.
-		var binaryNumber: string = (integer < 0) ? (integer >>> 0).toString(2) : integer.toString(2).padStart(DataSize.DOUBLEWORD, "0");
+		var binaryNumber: string = (integer < 0) ? 
+			(integer >>> 0).toString(2) : 
+			integer.toString(2).padStart(DataSizes.DOUBLEWORD, "0");
 
 		binaryNumber.split("").forEach((bit, index) => {
 			doubleword._value[index] = (bit === "0") ? 0 : 1;
