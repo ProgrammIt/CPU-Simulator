35f89a6fe3ebde8a23d09c0ab41c97f633150a09
Erik Burmester <erik.burmester@nextbeam.net>
2025-01-04 16:03:12 +0100

    Add StackOverflowError as a meaningful extension to the standard error

---
+++ src/types/errors/StackOverflowError.ts
@@ -0,0 +1,15 @@
+/**
+ * Error which gets thrown whenever a process tries to push to STACK but STACK reached maximum capacity.
+ * @author Erik Burmester <erik.burmester@nextbeam.net>
+ */
+export class StackOverflowError extends Error {
+    /**
+	 * Constructs a new instance with the given message.
+	 * @param description A short text describing the error and its cause.
+	 */
+    constructor(description: string) {
+		super(description);
+        // Set the prototype explicitly to enable typechecking.
+        Object.setPrototypeOf(this, StackOverflowError.prototype);
+    }
+}
