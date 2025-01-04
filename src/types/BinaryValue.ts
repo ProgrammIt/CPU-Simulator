d92df77e365cce4070adbf816546e4498aaf38b8
Erik Burmester <erik.burmester@nextbeam.net>
2025-01-04 16:02:18 +0100

    Add methods for retrieving different portions of a binary value.

--- src/types/BinaryValue.ts
+++ src/types/BinaryValue.ts
@@ -1,4 +1,5 @@
 import { Bit } from "./Bit";
+import { DataSizes } from "./DataSizes";
 
 /**
  * This class represents a generic binary value.
@@ -35,4 +36,68 @@ export class BinaryValue {
         }
 		this._value = newValue;
 	}
+
+    /**
+     * This method returns the least significant bit of this value.
+     * @returns The least significant bit.
+     */
+    public getLeastSignificantBit(): Bit {
+        return this._value[this._value.length - 1];
+    }
+
+    /**
+     * This method returns the most significant bit of this value.
+     * @returns The most significant bit.
+     */
+    public getMostSignificantBit(): Bit {
+        return this._value[0];
+    }
+
+    /**
+     * This method returns the least significant byte of this value.
+     * @returns The least significant byte.
+     */
+    public getLeastSignificantByte(): Array<Bit> {
+        if (this._value.length < 8) {
+            throw new Error("The value does not contain enough bits to retrieve a byte.");
+        }
+        return this._value.slice(-DataSizes.BYTE);
+    }
+
+    /**
+     * This method returns the least significant byte of this value.
+     * @returns The least significant byte.
+     */
+    public getMostSignificantByte(): Array<Bit> {
+        if (this._value.length < 8) {
+            throw new Error("The value does not contain enough bits to retrieve a byte.");
+        }
+        return this._value.slice(0, DataSizes.BYTE);
+    }
+
+    /**
+     * This method returns the last bits of the binary value.
+     * The number of bits returned depends on the argument passed.
+     * @param nbrOfBits 
+     * @returns 
+     */
+    public getLeastSignificantBits(nbrOfBits: number): BinaryValue {
+        if (nbrOfBits > this._value.length) {
+            throw new Error(`The value does not contain enough bits to retrieve a subset of ${nbrOfBits} bits.`);
+        }
+        return new BinaryValue(this._value.slice(-nbrOfBits));
+    }
+
+    /**
+     * This method returns the first bits of the binary value.
+     * The number of bits returned depends on the argument passed.
+     * @param nbrOfBits 
+     * @returns 
+     */
+    public getMostSignificantBits(nbrOfBits: number): BinaryValue {
+        if (nbrOfBits > this._value.length) {
+            throw new Error(`The value does not contain enough bits to retrieve a subset of ${nbrOfBits} bits.`);
+        }
+        return new BinaryValue(this._value.slice(0, nbrOfBits));
+    }
 }
