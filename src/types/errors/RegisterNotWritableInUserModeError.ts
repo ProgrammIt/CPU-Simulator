b6af29f155320e5a80ac7df754fd0a9a31a5e504
Erik Burmester <erik.burmester@nextbeam.net>
2025-01-04 16:03:55 +0100

    Add RegisterNotWritableInUserModeError as a meaningful extension to the standard error

---
+++ src/types/errors/RegisterNotWritableInUserModeError.ts
@@ -0,0 +1,15 @@
+/**
+ * Error which gets thrown whenever a process tries to write to a read-only register in user mode.
+ * @author Erik Burmester <erik.burmester@nextbeam.net>
+ */
+export class RegisterNotWritableInUserModeError extends Error {
+    /**
+	 * Constructs a new instance with the given message.
+	 * @param description A short text describing the error and its cause.
+	 */
+    constructor(description: string) {
+		super(description);
+        // Set the prototype explicitly to enable typechecking.
+        Object.setPrototypeOf(this, RegisterNotWritableInUserModeError.prototype);
+    }
+}
