fdd16f40e84ffd517591428fedef80903d885fda
Erik Burmester <erik.burmester@nextbeam.net>
2025-01-15 23:00:41 +0100

    Fix error in jump label declaration

--- assets/programs/examples/array.asm
+++ assets/programs/examples/array.asm
@@ -3,7 +3,7 @@
 MOV $0b0, %eax      ; Kopiere den Wert 0 in das Register EAX.
 MOV $0x64, %ebx      ; Kopiere den Wert 100 in das Register EBX.
 
-loop:
+.loop:
     MOV $0, *%ebx   ; Kopiere den Wert 0 an die Speicheradresse, auf die das Register EBX zeigt. Der Wert in EBX wird dabei als Speicheradresse verwendet.
     ADD $1, %eax    ; Inkrementiere den Wert im Register EAX.
     ADD $4, %ebx    ; Inkrementiere den Wert im Register EBX um 4 (Byte <=> 32 Bit <=> eine Zahl).
