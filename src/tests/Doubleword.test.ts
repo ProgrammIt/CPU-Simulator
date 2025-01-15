a36ea69dbb411fdac87f236d3d64ef2b175c9eb2
Erik Burmester <erik.burmester@nextbeam.net>
2025-01-15 23:01:48 +0100

    Add new testcases

--- src/tests/Doubleword.test.ts
+++ src/tests/Doubleword.test.ts
@@ -19,4 +19,20 @@ describe("Create doubleword from decimal integer values", () => {
     test("Create doubleword from positive decimal integer value", () => {
         expect(Doubleword.fromInteger(6).value.join("")).toBe("00000000000000000000000000000110");
     });
+
+    test("Check wether (8)_10 is smaller than (11)_10", () => {
+        expect(Doubleword.fromInteger(8).isSmallerThan(Doubleword.fromInteger(11))).toBe(true);
+    });
+
+    test("Check wether (-1)_10 is smaller than (1)_10", () => {
+        expect(Doubleword.fromInteger(-1).isSmallerThan(Doubleword.fromInteger(1))).toBe(true);
+    });
+
+    test("Check wether (-1)_10 is smaller than (-2)_10", () => {
+        expect(Doubleword.fromInteger(-1).isSmallerThan(Doubleword.fromInteger(-2))).toBe(false);
+    });
+
+    test("Check wether (100)_10 is smaller than (11)_10", () => {
+        expect(Doubleword.fromInteger(100).isSmallerThan(Doubleword.fromInteger(11))).toBe(false);
+    });
 });
