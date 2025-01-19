ed2b1743ea413ae541945108aecc387ed0672514
Erik Burmester <erik.burmester@nextbeam.net>
2025-01-19 20:06:43 +0100

    Change start value

--- assets/programs/examples/loop.asm
+++ assets/programs/examples/loop.asm
@@ -1,8 +1,8 @@
-MOV $0x64, %eax      ; Kopiere den Wert 100 in das Register eax.
+MOV $0x4, %eax      ; Kopiere den Wert 100 in das Register eax.
 
 .loop: 
     SUB $1, %eax     ; Subtrahiere den Wert 1 vom Wert im Register eax.
     CMP $0, %eax     ; Vergleiche den Inhalt von Register eax mit der 0.
-    JG loop         ; Springe zum Schleifenanfang, wenn der Wert im Register eax noch größer als 0 ist.
+    JG loop          ; Springe zum Schleifenanfang, wenn der Wert im Register eax noch größer als 0 ist.
 
 ; Ende des Programms. Das Ergebnis steht in Register eax.
