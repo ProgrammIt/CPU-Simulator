df66b319322b8fd98839236ca2a4458398d7019f
Erik Burmester <erik.burmester@nextbeam.net>
2025-01-19 21:09:10 +0100

    Add new event handlers and a custom menu to the application window

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, dialog, ipcMain, IpcMainEvent, Menu } from "electron";
+import { app, BrowserWindow, dialog, ipcMain, IpcMainEvent, IpcMainInvokeEvent, Menu, shell } from "electron";
 import path from 'path';
 import { Simulator } from "./simulator/Simulator";
 import { Doubleword } from "./types/Doubleword";
@@ -6,58 +6,184 @@ import { twosComplementToDecimal } from "./helper";
 import { NumberSystem } from "./types";
 import { PhysicalAddress } from "./types/PhysicalAddress";
 import { Byte } from "./types/Byte";
-import { readFileSync } from "original-fs";
 
 const createWindow = (win: BrowserWindow, simulator: Simulator) => {
-	// setting up the menu with just two items 
 	const menu = Menu.buildFromTemplate([
 		{
-		label: 'Menu',
-		submenu: [
-			{
-				label:'Open Assembly Program',
-				accelerator: 'CmdOrCtrl+O',
-				// this is the main bit hijack the click event 
-				click() {
-					// construct the select file dialog 
-					dialog.showOpenDialog({
-						properties: ["openFile", "createDirectory"],
-						filters: [{ name: "Assembly Files", extensions: ['asm'] }]
-					}).then(function(fileObj) {
-						if (!fileObj.canceled) {
-							win.webContents.send("open_program", fileObj.filePaths);
-							// TODO: Load assembly program into simulator!
-							// simulator.loadProgram(fileObj.filePaths);
-						}
-					}).catch((err) => win.webContents.send("on_error", err))
+			label: "App",
+			submenu: [
+				{
+					label: "Exit",
+					accelerator: "CmdOrCtrl+Q",
+					click() {
+						app.quit()
+					} 
 				}
-			},
-			{
-				label:'Exit',
-				accelerator: 'CmdOrCtrl+Q',
-				click() {
-					app.quit()
-				} 
-			}
-		]
+			]
+		},
+		{
+			label: "File",
+			submenu: [
+				{
+					label: "Open Assembly Program",
+					accelerator: "CmdOrCtrl+O",
+					click() {
+						dialog.showOpenDialog({
+							properties: ["openFile", "createDirectory"],
+							filters: [{ name: "Assembly Files", extensions: ['asm'] }]
+						}).then(function(fileObj) {
+							if (!fileObj.canceled) {
+								simulator.bootProcess(fileObj.filePaths[0]);
+								win.webContents.send("loaded_program", fileObj.filePaths);
+							}
+						}).catch((err) => win.webContents.send("on_error", err))
+					}
+				}
+			],
+		},
+		{
+			label: "Settings",
+			submenu: [
+				{
+					label: "Behavior",
+					submenu: [
+						{
+							label: "Physical RAM",
+							submenu : [
+								{
+									label: "Disable Auto Scroll",
+									click() {
+										if (simulator.autoScrollForPhysicalRAMEnabled) {
+											win.webContents.send("disable_auto_scroll_physical_ram");
+										}
+									}
+								},
+								{
+									label: "Enable Auto Scroll",
+									click() {
+										if (!simulator.autoScrollForPhysicalRAMEnabled) {
+											win.webContents.send("enable_auto_scroll_physical_ram");
+										}
+									}
+								}
+							]
+						},
+						{
+							label: "Virtual RAM",
+							submenu : [
+								{
+									label: "Disable Auto Scroll",
+									click() {
+										if (simulator.autoScrollForVirtualRAMEnabled) {
+											win.webContents.send("disable_auto_scroll_virtual_ram");
+										}
+									}
+								},
+								{
+									label: "Enable Auto Scroll",
+									click() {
+										if (!simulator.autoScrollForVirtualRAMEnabled) {
+											win.webContents.send("enable_auto_scroll_virtual_ram");
+										}
+									}
+								}
+							]
+						},
+						{
+							label: "Page Table",
+							submenu : [
+								{
+									label: "Disable Auto Scroll",
+									click() {
+										if (simulator.autoScrollForPageTableEnabled) {
+											win.webContents.send("disable_auto_scroll_page_table");
+										}
+									}
+								},
+								{
+									label: "Enable Auto Scroll",
+									click() {
+										if (!simulator.autoScrollForPageTableEnabled) {
+											win.webContents.send("enable_auto_scroll_page_table");
+										}
+									}
+								}
+							]
+						}
+					]
+				}
+			]
+		},
+		{
+			label: "Window",
+			submenu: [
+				{
+					label: "Minimize",
+					accelerator: "CmdOrCtrl+M",
+					click() {
+						if (win.isMinimizable() && !win.isMinimized()) {
+							win.minimize();
+						}
+					}
+				},
+				{
+					label: "Reload",
+					accelerator: "CmdOrCtrl+R",
+					click() {
+						win.webContents.reload()
+					}
+				},
+				{
+					label: "Force Reload",
+					accelerator: "CmdOrCtrl+Shift+R",
+					click() {
+						win.webContents.reloadIgnoringCache()
+					}
+				},
+				{
+					label: "Toggle Developer Tools",
+					accelerator: "CmdOrCtrl+Shift+I",
+					click() {
+						win.webContents.openDevTools();
+					}
+				}
+			]
+		},
+		{
+			label: "Help",
+			submenu: [
+				{
+					label: "Documentation",
+					accelerator: "CmdOrCtrl+H",
+					click() {
+						shell.openExternal("https://programmit.github.io/CPU-Simulator/")
+					}
+				},
+				{
+					label: "GitHub Repository",
+					click() {
+						shell.openExternal("https://github.com/ProgrammIt/CPU-Simulator")
+					}
+				}
+			]
 		}
 	])
 	Menu.setApplicationMenu(menu);
-
   	win.loadFile("./src/index.html");
 };
 
 app.whenReady().then(() => {
 	const win = new BrowserWindow({
+		title: "Ihme Core X1 Simulator",
 		width: 800,
 		height: 600,
 		webPreferences: {
       		preload: path.join(__dirname, 'preload.js')
-    	}
+    	},
+		icon: "./assets/img/icons/icon.png"
   	});
 
-	const simulator = Simulator.getInstance(Math.pow(2, 32));
-	// simulator.loadProgramm(readFileSync("./assets/programs/examples/for_loop.asm", "utf8"));    
+	const simulator = Simulator.getInstance(Math.pow(2, 32));    
 	
 	createWindow(win, simulator);
 
@@ -70,7 +196,7 @@ app.whenReady().then(() => {
 		if (BrowserWindow.getAllWindows().length === 0) createWindow(win, simulator);
   	});
 
-	ipcMain.handle("readRangeFromMainMemory", async (event: Electron.IpcMainInvokeEvent, fromPhysicalAddressHexString: string, toPhysicalAddressHexString: string): Promise<Map<string, string>> => {
+	ipcMain.handle("readRangeFromPhysicalMemory", async (event: Electron.IpcMainInvokeEvent, fromPhysicalAddressHexString: string, toPhysicalAddressHexString: string): Promise<Map<string, string>> => {
 		const tmp: Map<string, string> = new Map<string, string>();
 		const from: number = parseInt(fromPhysicalAddressHexString, 16);
 		const to: number = parseInt(toPhysicalAddressHexString, 16);
@@ -81,9 +207,26 @@ app.whenReady().then(() => {
 		return tmp;
 	});
 
-	ipcMain.handle("readFromMainMemory", async (event: Electron.IpcMainInvokeEvent, physicalAddressHexString: string): Promise<string> => {
+	ipcMain.handle("readFromPhysicalMemory", async (event: Electron.IpcMainInvokeEvent, physicalAddressHexString: string): Promise<string> => {
 		const physicalAddressDec: number = parseInt(physicalAddressHexString, 16);
 		const byte: Byte = simulator.mainMemory.readByteFrom(PhysicalAddress.fromInteger(physicalAddressDec));
+		return byte.toString();
+	});
+
+	ipcMain.handle("readRangeFromVirtualMemory", async (event: Electron.IpcMainInvokeEvent, fromVirtualAddressHexString: string, toVirtualAddressHexString: string): Promise<Map<string, string>> => {
+		const tmp: Map<string, string> = new Map<string, string>();
+		const from: number = parseInt(fromVirtualAddressHexString, 16);
+		const to: number = parseInt(toVirtualAddressHexString, 16);
+		for (let i = from; i <= to; ++i) {
+			const byte: Byte = simulator.core.mmu.readByteFrom(PhysicalAddress.fromInteger(i));
+			tmp.set(`0x${(i).toString(16)}`, byte.toString());
+		}
+		return tmp;
+	});
+
+	ipcMain.handle("readFromVirtualMemory", async (event: Electron.IpcMainInvokeEvent, virtualAddressHexString: string): Promise<string> => {
+		const virtualAddressDec: number = parseInt(virtualAddressHexString, 16);
+		const byte: Byte = simulator.core.mmu.readByteFrom(PhysicalAddress.fromInteger(virtualAddressDec));
 		return byte.toString();
 	});
 
@@ -95,123 +238,150 @@ app.whenReady().then(() => {
 		return tmp;
 	});
 
-	ipcMain.handle("readEAX", async (event, basis): Promise<string> => {
+	ipcMain.handle("readEAX", async (event: IpcMainInvokeEvent, basis: NumberSystem): Promise<string> => {
 		const content: Doubleword = simulator.core.eax.content;
-		var result: string = twosComplementToDecimal(content).toString(basis);
+		var result: string;
 		if (basis === NumberSystem.HEX) {
-			result = `0x${result}`;
-		} 
-		if (basis === NumberSystem.BIN) {
-			result = `0b${result}`;
+			result = `0x${twosComplementToDecimal(content).toString(16)}`;
+		} else if (basis === NumberSystem.DEC) {
+			result = `${twosComplementToDecimal(content)}`;
+		} else {
+			result = content.toString(true);
 		}
 		return result;
 	});
 
-	ipcMain.handle("readEBX", async (event, basis): Promise<string> => {
+	ipcMain.handle("readEBX", async (event: IpcMainInvokeEvent, basis: NumberSystem): Promise<string> => {
 		const content: Doubleword = simulator.core.ebx.content;
-		var result: string = twosComplementToDecimal(content).toString(basis);
+		var result: string;
 		if (basis === NumberSystem.HEX) {
-			result = `0x${result}`;
-		} 
-		if (basis === NumberSystem.BIN) {
-			result = `0b${result}`;
+			result = `0x${twosComplementToDecimal(content).toString(16)}`;
+		} else if (basis === NumberSystem.DEC) {
+			result = `${twosComplementToDecimal(content)}`;
+		} else {
+			result = content.toString(true);
 		}
 		return result;
 	});
 
-	ipcMain.handle("readEIP", async (event, basis): Promise<string> => {
+	ipcMain.handle("readEDX", async (event: IpcMainInvokeEvent, basis: NumberSystem): Promise<string> => {
+		const content: Doubleword = simulator.core.edx.content;
+		var result: string = twosComplementToDecimal(content).toString(basis);
+		var result: string;
+		if (basis === NumberSystem.HEX) {
+			result = `0x${twosComplementToDecimal(content).toString(16)}`;
+		} else if (basis === NumberSystem.DEC) {
+			result = `${twosComplementToDecimal(content)}`;
+		} else {
+			result = content.toString(true);
+		}
+		return result;
+	});
+
+	ipcMain.handle("readEIP", async (event: IpcMainInvokeEvent, basis: NumberSystem): Promise<string> => {
 		const content = simulator.core.eip.content;
-		var result: string = twosComplementToDecimal(content).toString(basis);
+		var result: string = parseInt(content.toString(), 2).toString(basis);
+		var result: string;
 		if (basis === NumberSystem.HEX) {
-			result = `0x${result}`;
-		} 
-		if (basis === NumberSystem.BIN) {
-			result = `0b${result}`;
+			result = `0x${parseInt(content.toString(), 2).toString(16)}`;
+		} else if (basis === NumberSystem.DEC) {
+			result = `${parseInt(content.toString(), 2)}`;
+		} else {
+			result = content.toString(true);
 		}
 		return result;
 	});
 
-	ipcMain.handle("readEFLAGS", async (event, basis): Promise<string> => {
+	ipcMain.handle("readEFLAGS", async (event: IpcMainInvokeEvent, basis: NumberSystem): Promise<string> => {
 		const content = simulator.core.eflags.content;
 		return content.toString();
 	});
 
-	ipcMain.handle("readEIR", async (event): Promise<string> => {
+	ipcMain.handle("readEIR", async (event: IpcMainInvokeEvent, asInstruction: boolean): Promise<string> => {
+		if (asInstruction) {
+			// TODO
+		}
 		const content = simulator.core.eir.content;
-		return content.toString();
+		return content.toString(true);
 	});
 
-	ipcMain.handle("readNPTP", async (event, basis): Promise<string> => {
+	ipcMain.handle("readNPTP", async (event: IpcMainInvokeEvent, basis: NumberSystem): Promise<string> => {
 		const content = simulator.core.nptp.content;
-		var result: string = twosComplementToDecimal(content).toString(basis);
+		var result: string;
 		if (basis === NumberSystem.HEX) {
-			result = `0x${result}`;
-		} 
-		if (basis === NumberSystem.BIN) {
-			result = `0b${result}`;
+			result = `0x${parseInt(content.toString(), 2).toString(16)}`;
+		} else if (basis === NumberSystem.DEC) {
+			result = `${parseInt(content.toString(), 2)}`;
+		} else {
+			result = content.toString(true);
 		}
 		return result;
 	});
 
-	ipcMain.handle("readVMPTR", async (event, basis): Promise<string> => {
+	ipcMain.handle("readVMPTR", async (event: IpcMainInvokeEvent, basis: NumberSystem): Promise<string> => {
 		const content = simulator.core.vmtpr.content;
-		var result: string = twosComplementToDecimal(content).toString(basis);
+		var result: string;
 		if (basis === NumberSystem.HEX) {
-			result = `0x${result}`;
-		} 
-		if (basis === NumberSystem.BIN) {
-			result = `0b${result}`;
+			result = `0x${parseInt(content.toString(), 2).toString(16)}`;
+		} else if (basis === NumberSystem.DEC) {
+			result = `${parseInt(content.toString(), 2)}`;
+		} else {
+			result = content.toString(true);
 		}
 		return result;
 	});
 
-	ipcMain.handle("readESP", async (event, basis): Promise<string> => {
+	ipcMain.handle("readESP", async (event: IpcMainInvokeEvent, basis: NumberSystem): Promise<string> => {
 		const content = simulator.core.esp.content;
-		var result: string = twosComplementToDecimal(content).toString(basis);
+		var result: string;
 		if (basis === NumberSystem.HEX) {
-			result = `0x${result}`;
-		} 
-		if (basis === NumberSystem.BIN) {
-			result = `0b${result}`;
+			result = `0x${parseInt(content.toString(), 2).toString(16)}`;
+		} else if (basis === NumberSystem.DEC) {
+			result = `${parseInt(content.toString(), 2)}`;
+		} else {
+			result = content.toString(true);
 		}
 		return result;
 	});
 
-	ipcMain.handle("readITP", async (event, basis): Promise<string> => {
+	ipcMain.handle("readITP", async (event: IpcMainInvokeEvent, basis: NumberSystem): Promise<string> => {
 		const content = simulator.core.itp.content;
-		var result: string = twosComplementToDecimal(content).toString(basis);
+		var result: string;
 		if (basis === NumberSystem.HEX) {
-			result = `0x${result}`;
-		} 
-		if (basis === NumberSystem.BIN) {
-			result = `0b${result}`;
+			result = `0x${parseInt(content.toString(), 2).toString(16)}`;
+		} else if (basis === NumberSystem.DEC) {
+			result = `${parseInt(content.toString(), 2)}`;
+		} else {
+			result = content.toString(true);
 		}
 		return result;
 	});
 
-	ipcMain.handle("readGPTP", async (event, basis): Promise<string> => {
+	ipcMain.handle("readGPTP", async (event: IpcMainInvokeEvent, basis: NumberSystem): Promise<string> => {
 		if (simulator.core.gptp === null) {
-			return " ";
+			return "-";
 		}
 		const content = simulator.core.gptp.content;
-		var result: string = twosComplementToDecimal(content).toString(basis);
+		var result: string;
 		if (basis === NumberSystem.HEX) {
-			result = `0x${result}`;
-		} 
-		if (basis === NumberSystem.BIN) {
-			result = `0b${result}`;
+			result = `0x${parseInt(content.toString(), 2).toString(16)}`;
+		} else if (basis === NumberSystem.DEC) {
+			result = `${parseInt(content.toString(), 2)}`;
+		} else {
+			result = content.toString(true);
 		}
 		return result;
 	});
 
-	ipcMain.handle("readPTP", async (event, basis): Promise<string> => {
+	ipcMain.handle("readPTP", async (event: IpcMainInvokeEvent, basis: NumberSystem): Promise<string> => {
 		const content = simulator.core.ptp.content;
-		var result: string = twosComplementToDecimal(content).toString(basis);
+		var result: string;
 		if (basis === NumberSystem.HEX) {
-			result = `0x${result}`;
-		} 
-		if (basis === NumberSystem.BIN) {
-			result = `0b${result}`;
+			result = `0x${parseInt(content.toString(), 2).toString(16)}`;
+		} else if (basis === NumberSystem.DEC) {
+			result = `${parseInt(content.toString(), 2)}`;
+		} else {
+			result = content.toString(true);
 		}
 		return result;
 	});
@@ -226,5 +396,35 @@ app.whenReady().then(() => {
 			}
 		}
 		return resultOfCycle;
+	});
+
+	ipcMain.handle("on_disable_auto_scroll_physical_ram", async (): Promise<void> => {
+		simulator.autoScrollForPhysicalRAMEnabled = false;
+		return;
+	});
+
+	ipcMain.handle("on_ensable_auto_scroll_physical_ram", async (): Promise<void> => {
+		simulator.autoScrollForPhysicalRAMEnabled = true;
+		return;
+	});
+
+	ipcMain.handle("on_disable_auto_scroll_virtual_ram", async (): Promise<void> => {
+		simulator.autoScrollForVirtualRAMEnabled = false;
+		return;
+	});
+
+	ipcMain.handle("on_ensable_auto_scroll_virtual_ram", async (): Promise<void> => {
+		simulator.autoScrollForVirtualRAMEnabled = true;
+		return;
+	});
+
+	ipcMain.handle("on_disable_auto_scroll_page_table", async (): Promise<void> => {
+		simulator.autoScrollForPageTableEnabled = false;
+		return;
+	});
+
+	ipcMain.handle("on_enable_auto_scroll_page_table", async (): Promise<void> => {
+		simulator.autoScrollForPageTableEnabled = true;
+		return;
 	});
 });
