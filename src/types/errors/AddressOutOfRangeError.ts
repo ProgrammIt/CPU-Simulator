07fe769f0177d324badb628964a9ffd6cbfc6016
Erik Burmester <erik.burmester@nextbeam.net>
2025-01-22 15:01:13 +0100

    Introduce new Error type for addresses, which are out of the range of allowed values

---
+++ src/types/errors/AddressOutOfRangeError.ts
@@ -0,0 +1,15 @@
+/**
+ * Error which gets thrown whenever a given physical memory address is out of the allowed range.
+ * @author Erik Burmester <erik.burmester@nextbeam.net>
+ */
+export class AddressOutOfRangeError extends Error {
+    /**
+	 * Constructs a new instance with the given message.
+	 * @param description A short text describing the error and its cause.
+	 */
+    constructor(description: string) {
+        super(description);
+        // Set the prototype explicitly to enable typechecking.
+        Object.setPrototypeOf(this, AddressOutOfRangeError.prototype);
+    }
+}
