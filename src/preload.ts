4d80b3da045c236699193ad78b5001f41c1c01bc
Erik Burmester <erik.burmester@nextbeam.net>
2025-03-14 23:12:11 +0100

    Add webpack template for electron forge

--- src/preload.ts
+++ src/preload.ts
@@ -1,3 +1,6 @@
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
 import { contextBridge, ipcRenderer } from 'electron';
 import { NumberSystems } from './enumerations/NumberSystems';
 
@@ -5,7 +8,7 @@ declare global {
 	interface Window {
 		mainMemory: any,
 		simulator: any
-  	}
+	}
 }
 
 contextBridge.exposeInMainWorld("mainMemory", {
