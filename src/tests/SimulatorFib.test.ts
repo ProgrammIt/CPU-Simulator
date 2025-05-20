640709538a6d65b20333694f04193942aa6d7b6d
Laurin Gehlenborg <lauringehlenborg@gmail.com>
2025-05-20 15:17:10 +0200

    add SimulatorFib.test.ts

---
+++ src/tests/SimulatorFib.test.ts
@@ -0,0 +1,16 @@
+import { SimulationController } from "../main/simulator/SimulationController";
+import { DoubleWord } from "../types/binary/DoubleWord";
+
+describe('Test Simulator', () => {
+    const simulator: SimulationController = SimulationController.getInstance(
+        Math.pow(2, 32), 
+        "./settings/language_definition.json",
+        "./assembly"
+    );
+    simulator.bootProcess("./assembly/examples/fib.asm");
+    
+    test('Test if PTP register points to a valid page table entry', () => {
+        const expectedOutput: DoubleWord = new DoubleWord([1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+        expect(simulator.mainMemory.readDoublewordFrom(simulator.core.ptp.content)).toEqual(expectedOutput);
+    });
+});
