7185e0b0f02ac2d48cbe8442534269386d2aabf0
Erik Burmester <erik.burmester@nextbeam.net>
2025-05-22 07:58:07 +0200

    Add abstract methods to the component's interfaces

--- src/renderer/Component.ts
+++ src/renderer/Component.ts
@@ -1,3 +1,6 @@
+import { Message } from "./Message";
+import { LeafNode } from "./RenderTree";
+
 /**
  * This class represents a generic component of the graphical user interface.
  * A component is a bundle of a TypeScript class and an accompanied HTML template file.
@@ -12,9 +15,16 @@ export abstract class Component {
     protected _template: string;
 
     /**
-     * This member stores a mapping between keys in the template file and their associated values.
+     * This member stores the current mapping between keys in the template file and their associated values
+     * as displayed in the graphical user interface.
      */    
     protected _args: Map<string, string>;
+
+    /**
+     * This member stores a reference to the leaf node associated with this component.
+     * The reference is used to send messages to other nodes in the tree.
+     */
+    protected _associatedNode: LeafNode | null;
 
     /**
      * Constructs a generic component from the given mapping between keys in the template file and their 
@@ -24,11 +34,52 @@ export abstract class Component {
     public constructor(args: Map<string, string>) {
         this._args = args;
         this._template = "";
+        this._associatedNode = null;
+    }
+
+    /**
+     * This accessor allows late setting of the leaf node associated with this component.
+     */
+    public set associatedNode(node: LeafNode) {
+        this._associatedNode = node;
     }
 
     /**
      * This method creates a HTML string from its template and replaces the placeholders with
      * its current arguments.
      */
-    public abstract render(): string;
+    public abstract init(): string;
+
+    /**
+     * This method updates the rendered component.
+     */
+    public abstract update(): void;
+
+    /**
+     * This method processes the message received by the render node, this component is attached to.
+     * @param msg A message received by the render node.
+     */
+    public abstract processMessage(msg: Message): void;
+
+    /**
+     * This method emits a message to the associated leaf node. The leaf node will send the message to its
+     * parent node, which will forward the message to other nodes in the render tree.
+     * @param msg The message to send.
+     */
+    protected emitMessage(msg: Message): void {
+        if (this._associatedNode) {
+            this._associatedNode.receiveMessage(msg);
+        }
+    }
+
+    /**
+     * This method checks, whether a given key in the list of arguments is present. If it is present
+     * if checks whether the value differs from the given one.
+     * @param argKey The key to look up in the list of arguments.
+     * @param value The value to check for changes.
+     * @returns 
+     */
+    protected differ(argKey: string, value: string): boolean {
+        return this._args.has(argKey) && (value !== this._args.get(argKey))
+    }
 }
