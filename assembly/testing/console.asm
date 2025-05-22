e5f3b9c7e1fcd0b5df14f35436eff221637a5ed2
Laurin Gehlenborg <lauringehlenborg@gmail.com>
2025-05-22 21:50:05 +0200

    assembly testing

--- assembly/testing/console.asm
+++ assembly/testing/console.asm
@@ -2,27 +2,27 @@ NOP ; enter kernel mode (requires patched NOP instruction)
 
 MOV $0x100, %esp
 
-; print 123
-PUSH $123
-DEV $0b01000, *%esp  ; 00001000 - console_print_number(number=op2)
-
-; print 'aaaa'
-MOV $0, @0x108 ; null-termination
-MOV $0b01100001011000010110000101100001, @0x104    ; "aaaa"
-PUSH $4
-PUSH $1000
-DEV $0b0011, $0     ; 00000011 - io_write_buffer (fd=op2, buffer=stack, b_size=stack) -> bytes_written=eax
+;; print 123
+;PUSH $123
+;DEV $0b01000, *%esp  ; 00001000 - console_print_number(number=op2)
+;
+;; print 'aaaa'
+;MOV $0, @0x108 ; null-termination
+;MOV $0b01100001011000010110000101100001, @0x104    ; "aaaa"
+;PUSH $4
+;PUSH $0x104
+;DEV $0b0011, $0     ; 00000011 - io_write_buffer (fd=op2, buffer=stack, b_size=stack) -> bytes_written=eax
 
 ; read up to 100 bytes
-PUSH $0x8000        ; buffer address
 PUSH $100           ; buffer length
+PUSH $0x8000        ; buffer address
 DEV $0b0010, $0     ; 00000010 - io_read_buffer (fd=op2, buffer_ptr=stack, buffer_size=stack) -> bytes_read=eax
 
 ; print amount of bytes read
 DEV $0b01000, %eax  ; 00001000 - console_print_number(number=op2)
 
 ; print the read buffer
-PUSH @0x8000        ; buffer address
+PUSH $0x8000        ; buffer address
 PUSH %eax           ; buffer length
 DEV $0b0011, $0     ; 00000011 - io_write_buffer (fd=op2, buffer=stack, b_size=stack) -> bytes_written=eax
 
--- assembly/testing/pop.asm
+++ assembly/testing/pop.asm
@@ -1,2 +1,23 @@
-PUSH $63
-POP %ebx
+NOP
+
+; immediate
+PUSH $1
+DEV $0b01000, *%esp  ; 00001000 - console_print_number(number=op2)
+
+PUSH $0x2
+DEV $0b01000, *%esp  ; 00001000 - console_print_number(number=op2)
+
+PUSH $0b011
+DEV $0b01000, *%esp  ; 00001000 - console_print_number(number=op2)
+
+; address
+PUSH @4
+DEV $0b01000, *%esp  ; 00001000 - console_print_number(number=op2)
+
+PUSH @0x5
+DEV $0b01000, *%esp  ; 00001000 - console_print_number(number=op2)
+
+PUSH @0b0101
+DEV $0b01000, *%esp  ; 00001000 - console_print_number(number=op2)
+
+NOP
